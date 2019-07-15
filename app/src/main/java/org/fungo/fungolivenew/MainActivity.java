f737f556a5a21b9ddf3f62cf22e38f3ee7d996f5
yangqingyuan <yangqingyuan@yuntutv.net>
2019-07-15 14:55:56 +0800

    base

--- app/src/main/java/org/fungo/fungolivenew/MainActivity.java
+++ app/src/main/java/org/fungo/fungolivenew/MainActivity.java
@@ -2,13 +2,26 @@ package org.fungo.fungolivenew;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.alibaba.fastjson.JSON;
+import com.zhy.adapter.recyclerview.MultiItemTypeAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @BindView(R.id.recycleView)
+    RecyclerView recycleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
